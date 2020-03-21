414cc58500aaac53121e6ee33f6dab135e50735f
Maxime Doré <m.dore@cameleonmedia.com>
2020-03-21 18:56:02 -0400

    TDS Unity Tutorial 01
    https://www.youtube.com/watch?v=pSN2x3dPgYw

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
-{
+[RequireComponent (typeof (CharacterController))]
+public class PlayerController : MonoBehaviour {
+
+    // Handling
+    public float rotationSpeed = 450;
+    public float walkSpeed = 5;
+    public float runSpeed = 8;
+
+    // System
+    private Quaternion targetRotation;
+
+    // Components
+    private CharacterController controller;
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        if (input != Vector3.zero) {
+            targetRotation = Quaternion.LookRotation(input);
+            transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
+        }
+
+        Vector3 motion = input;
+        motion += Vector3.up * -8;
+        motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1) ? .7f:1;
+        motion *= (Input.GetButton("Run")) ? runSpeed:walkSpeed;
+        controller.Move(motion * Time.deltaTime);
     }
 }
