585e95179005acc3844fe906ed6baaf97e94671a
Maxime Doré <maximedore43@gmail.com>
2020-03-22 12:46:07 -0400

    Added basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float movementSpeed = 5f;
+
+    // Components
+    Rigidbody2D rb;
+
+    // System
+    Vector2 movement;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() {
+        // Movement
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
